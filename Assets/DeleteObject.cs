5e1edefafbfa11fb4d96d948678189f544352c96
Makiya Tsutsui <maxiya@earley.jp>
2018-07-09 20:14:56 +0900

    Lesson6 課題提出
    StepAsideUnityChanを作成

---
+++ Assets/DeleteObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// 通過したオブジェクトを破棄
+		if (MyCameraController.unitychan.transform.position.z > this.transform.position.z) {
+			Destroy (this.gameObject);
+		}
+	}
+}
